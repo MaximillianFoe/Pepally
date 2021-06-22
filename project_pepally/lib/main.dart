753c3dd3f608f0cdf547e08830aab90c7dddf50d
MaximillianFoe <cicoban@yandex.com>
2021-06-22 15:39:02 +0300

    All files from previous repository are added.

--- project_pepally/lib/main.dart
+++ project_pepally/lib/main.dart
@@ -1,113 +1,353 @@
+import 'dart:convert';
+import 'dart:math';
+import 'package:http/http.dart' as http;
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-void main() {
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print('Handling a background message ${message.messageId}');
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  if (!kIsWeb) {
+    var channel = const AndroidNotificationChannel(
+      'high_importance_channel', // id
+      'High Importance Notifications', // title
+      'This channel is used for important notifications.', // description
+      importance: Importance.high,
+    );
+
+    await flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(channel);
+
+    await FirebaseMessaging.instance
+        .setForegroundNotificationPresentationOptions(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+  }
+
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        textTheme: Theme.of(context).textTheme.apply(
+              bodyColor: Colors.black,
+              displayColor: Colors.black,
+            ),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyStatefulWidget(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
+int _messageCount = 0;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
+String constructFCMPayload(String token) {
+  _messageCount++;
+  return jsonEncode({
+    'token': token,
+    'data': {
+      'via': 'FlutterFire Cloud Messaging!',
+      'count': _messageCount.toString(),
+    },
+    'notification': {
+      'title': 'Hello FlutterFire!',
+      'body': 'This notification (#$_messageCount) was created via FCM!',
+    },
+  });
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
 
-  void _incrementCounter() {
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  String _title = 'Home';
+  int _selectedIndex = 0;
+  static List<Widget> _widgetOptions = <Widget>[
+    MyHomePage(),
+    SettingsPage(),
+  ];
+
+  void _onItemTapped(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _title = "Pepally"; // W.I.P.
+      _selectedIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Colors.lightBlue,
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(_title),
+        backgroundColor: Colors.red,
+        centerTitle: true,
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+int yanitIndex = 0;
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<String> yanitlar = [
+    'Dil / Language / Язык',
+    'Bugün, aşk ve ilişki hayatınızda biraz daha temkinli olmalısın. Onu her konuda doğru anlamda anladığından da emin misin?',
+    'Aşk ve ilişki hayatınızda hakimiyeti eline almak ve birlikteliğinizi dilediğiniz gibi yönlendirmek isteyebilirsin. Bu isteğinin denge içerisindeki bir ilişkiye ne şekilde fayda edeceğini gözden geçirmelisin.',
+    'Duygularınızın bugün biraz düzensiz olduğunu görebilirsiniz. Güne, daha önce hiç yapmadığınız bir şekilde başlamanızı sağlayan doğal bir elektrik hissi var. Kalbiniz her zaman doğruyu söyler.',
+    'Duygularınız çoğu zaman rasyonel düşüncelerinize galip geliyor ve sonunda derinlerde duyguların beslediği bir karmaşa ortaya çıkıyor. Çok da mantıklı düşünerek kalbinizi görmezden gelmeyin. Amacınız bu ikisi arasında dengeyi bulmak.',
+    'Duygusal olarak kötü hissetmeyin, sezgileriniz güçlü durumda. Sevgilinizin hislerini bu sezgiler ile anlamaya çalışın. Bazen sadece eğlenmek gerekir, siz de rahatlayın ve birlikte eğlenin!',
+    'There are some expenses that you have put on your mind for a long time, today ask yourself how much you really need them and cancel some of them. Make a small donation to a charity today and you will feel so much better.',
+    'We are entering periods that can disrupt the balance of your cash flow, you should definitely sit at the desk and make a budget calculation, do not act without being sure of your income and expense balance for the next 3 months.',
+    'Today can be a day when you have a high sense of helping, supporting and serving others, and you can take part in charitable work.',
+    'Your self-confidence is quite high today, but it may not do you much good, your contacts may suspect their support. Act calmly, take a step back, and be more concerned with your health than money.',
+    'You should pay attention to the details of our financial resources this period. In addition, you can decide on a matter that has been keeping you busy for a long time in financial matters and expenses, ask your relatives or spouse for help while making a decision.',
+    'Не стесняйтесь задавать людям вопросы, подчеркнув свою любопытную личность сегодня. ',
+    'Сегодня вы можете осознать, что вещи, которые вы никогда раньше не замечали, всегда рядом, просто постарайтесь быть немного осторожнее.',
+    'Вы можете попробовать быть более активными и уступчивыми в двусторонних отношениях сегодня, начать внимательнее слушать и задавать вопросы людям, с которыми вы согласны.',
+    'Просто сосредоточьтесь на своей работе сегодня, уходя от всех отвлекающих факторов и социальных сетей, вы почувствуете себя счастливее.',
+    'Прогуляйтесь сегодня в одиночестве, включите песни, которые вы слушали в детстве, и представьте игры, в которые вы играли тогда.'
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print('A new onMessageOpenedApp event was published!');
+    });
+  }
+
+  Future<void> sendPushMessage() async {
+    try {
+      await http.post(
+        Uri.parse('https://api.rnfirebase.io/messaging/send'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: constructFCMPayload(
+            "BCNZidrD-kVFZPzkX0SlD4ydG7P-UqPimuoh-hK8Lxaoqz_KIDf7zAz6Coo3RI8zKd9AGHzwJBAgoZszqGqBYHM"),
+      );
+      print('FCM request for device sent!');
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> onActionSelected(String value) async {
+    switch (value) {
+      case 'subscribe':
+        {
+          print(
+              'FlutterFire Messaging Example: Subscribing to topic "fcm_test".');
+          await FirebaseMessaging.instance.subscribeToTopic('fcm_test');
+          print(
+              'FlutterFire Messaging Example: Subscribing to topic "fcm_test" successful.');
+        }
+        break;
+      case 'unsubscribe':
+        {
+          print(
+              'FlutterFire Messaging Example: Unsubscribing from topic "fcm_test".');
+          await FirebaseMessaging.instance.unsubscribeFromTopic('fcm_test');
+          print(
+              'FlutterFire Messaging Example: Unsubscribing from topic "fcm_test" successful.');
+        }
+        break;
+      default:
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlue,
+      body: Center(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 40.0),
+              child: ListTile(
+                leading: Icon(Icons.account_circle, color: Colors.redAccent),
+                title: Text(
+                  'Türkçe',
+                  style: TextStyle(
+                    color: Colors.redAccent,
+                  ),
+                ),
+                onTap: () {
+                  setState(() {
+                    yanitIndex = Random().nextInt(5) + 1;
+                  });
+                },
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 40.0),
+              child: ListTile(
+                leading: Icon(Icons.account_circle, color: Colors.green),
+                title: Text(
+                  'English',
+                  style: TextStyle(
+                    color: Colors.green,
+                  ),
+                ),
+                onTap: () {
+                  setState(() {
+                    yanitIndex = Random().nextInt(5) + 6;
+                  });
+                },
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 40.0),
+              child: ListTile(
+                leading: Icon(Icons.account_circle, color: Colors.blue),
+                title: Text(
+                  'Ру́сский',
+                  style: TextStyle(
+                    color: Colors.blue,
+                  ),
+                ),
+                onTap: () {
+                  setState(() {
+                    yanitIndex = Random().nextInt(5) + 11;
+                  });
+                },
+              ),
+            ),
+            Container(
+                padding: EdgeInsets.symmetric(horizontal: 25),
+                height: 150,
+                width: double.infinity,
+                child: Padding(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 5.0, horizontal: 20.0),
+                    child: Text(yanitlar[yanitIndex],
+                        style: TextStyle(fontSize: 16),
+                        textAlign: TextAlign.center))),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SettingsPage extends StatefulWidget {
+  @override
+  _SettingsPage createState() => _SettingsPage();
+}
+
+class _SettingsPage extends State<SettingsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlue,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 40.0),
+              child: ListTile(
+                leading:
+                    Icon(Icons.announcement_rounded, color: Colors.redAccent),
+                title: Text(
+                  'Quit Smoking',
+                  style: TextStyle(
+                    color: Colors.redAccent,
+                  ),
+                ),
+                onTap: () {
+                  FirebaseMessaging.instance.subscribeToTopic('QuitSmoking');
+                },
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 40.0),
+              child: ListTile(
+                leading: Icon(Icons.announcement_rounded, color: Colors.green),
+                title: Text(
+                  'Study Exams',
+                  style: TextStyle(
+                    color: Colors.green,
+                  ),
+                ),
+                onTap: () {
+                  FirebaseMessaging.instance.subscribeToTopic('StudyExams');
+                },
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 40.0),
+              child: ListTile(
+                leading:
+                    Icon(Icons.announcement_rounded, color: Colors.pinkAccent),
+                title: Text(
+                  'Love Problems',
+                  style: TextStyle(
+                    color: Colors.pinkAccent,
+                  ),
+                ),
+                onTap: () {
+                  FirebaseMessaging.instance.subscribeToTopic('LoveProblems');
+                },
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
