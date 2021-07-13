234f8697bd6506e05a61810d3cbaf7c257f214dd
MaximillianFoe <cicoban@yandex.com>
2021-07-13 19:16:47 +0300

    Splash screen added with animated text!

--- project_pepally/lib/main.dart
+++ project_pepally/lib/main.dart
@@ -1,12 +1,14 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:toggle_switch/toggle_switch.dart';
 
-final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
 
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   await Firebase.initializeApp();
@@ -16,7 +18,47 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-  runApp(MyApp());
+  runApp(MaterialApp(home: SplashScreen()));
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreen createState() => _SplashScreen();
+}
+
+class _SplashScreen extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 5), () {
+      Navigator.push(context, MaterialPageRoute(builder: (context) => MyApp()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            TextLiquidFill(
+                text: 'Pepally',
+                waveColor: Colors.blueAccent,
+                boxBackgroundColor: Colors.purpleAccent,
+                textStyle: TextStyle(
+                  fontSize: 72.0,
+                  fontWeight: FontWeight.normal,
+                ),
+              boxHeight: (MediaQuery.of(context).size.height),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -26,7 +68,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         textTheme: Theme.of(context).textTheme.apply(
               bodyColor: Colors.black,
-              displayColor: Colors.black,
+              displayColor: Colors.purpleAccent,
             ),
       ),
       home: MyStatefulWidget(),
@@ -93,12 +135,13 @@ class _MyHomePageState extends State<MyHomePage> {
       print('A new onMessageOpenedApp event was published!');
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Home"),
-        backgroundColor: Colors.black54,
+        backgroundColor: Colors.purpleAccent,
         centerTitle: true,
         elevation: 0.0,
       ),
@@ -117,12 +160,13 @@ class SettingsPage extends StatefulWidget {
 
 class _SettingsPage extends State<SettingsPage> {
   int subStatus = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Settings"),
-        backgroundColor: Colors.black54,
+        backgroundColor: Colors.purpleAccent,
         centerTitle: true,
         elevation: 0.0,
       ),
@@ -133,7 +177,8 @@ class _SettingsPage extends State<SettingsPage> {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             Padding(
-              padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 80.0),
+              padding:
+                  const EdgeInsets.symmetric(vertical: 5.0, horizontal: 80.0),
               child: Text(
                 'What would like to do?',
                 style: TextStyle(fontWeight: FontWeight.normal),
@@ -148,14 +193,21 @@ class _SettingsPage extends State<SettingsPage> {
               inactiveFgColor: Colors.white,
               totalSwitches: 2,
               labels: ['Subscribe', 'Unsubscribe'],
-              icons: [CupertinoIcons.hand_thumbsup, CupertinoIcons.hand_thumbsdown],
-              activeBgColors: [[Colors.blueAccent],[Colors.blueAccent]],
+              icons: [
+                CupertinoIcons.hand_thumbsup,
+                CupertinoIcons.hand_thumbsdown
+              ],
+              activeBgColors: [
+                [Colors.blueAccent],
+                [Colors.blueAccent]
+              ],
               onToggle: (index) {
                 subStatus = index;
               },
             ),
             Padding(
-              padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 80.0),
+              padding:
+                  const EdgeInsets.symmetric(vertical: 5.0, horizontal: 80.0),
               child: Text(
                 'Select Topics:',
                 style: TextStyle(fontWeight: FontWeight.normal),
@@ -164,8 +216,8 @@ class _SettingsPage extends State<SettingsPage> {
             Card(
               margin: EdgeInsets.symmetric(vertical: 1.0, horizontal: 80.0),
               child: ListTile(
-                leading:
-                    Icon(CupertinoIcons.checkmark_seal_fill, color: Colors.redAccent),
+                leading: Icon(CupertinoIcons.checkmark_seal_fill,
+                    color: Colors.redAccent),
                 title: Text(
                   'Quit Smoking',
                   style: TextStyle(
@@ -176,8 +228,9 @@ class _SettingsPage extends State<SettingsPage> {
                   if (subStatus == 0) {
                     FirebaseMessaging.instance.subscribeToTopic('QuitSmoking');
                   }
-                  if (subStatus == 1){
-                    FirebaseMessaging.instance.unsubscribeFromTopic('QuitSmoking');
+                  if (subStatus == 1) {
+                    FirebaseMessaging.instance
+                        .unsubscribeFromTopic('QuitSmoking');
                   }
                 },
               ),
@@ -196,8 +249,9 @@ class _SettingsPage extends State<SettingsPage> {
                   if (subStatus == 0) {
                     FirebaseMessaging.instance.subscribeToTopic('StudyExams');
                   }
-                  if (subStatus == 1){
-                    FirebaseMessaging.instance.unsubscribeFromTopic('StudyExams');
+                  if (subStatus == 1) {
+                    FirebaseMessaging.instance
+                        .unsubscribeFromTopic('StudyExams');
                   }
                 },
               ),
@@ -205,8 +259,8 @@ class _SettingsPage extends State<SettingsPage> {
             Card(
               margin: EdgeInsets.symmetric(vertical: 1.0, horizontal: 80.0),
               child: ListTile(
-                leading:
-                    Icon(CupertinoIcons.heart_slash_fill, color: Colors.pinkAccent),
+                leading: Icon(CupertinoIcons.heart_slash_fill,
+                    color: Colors.pinkAccent),
                 title: Text(
                   'Love Problems',
                   style: TextStyle(
@@ -217,8 +271,9 @@ class _SettingsPage extends State<SettingsPage> {
                   if (subStatus == 0) {
                     FirebaseMessaging.instance.subscribeToTopic('LoveProblems');
                   }
-                  if (subStatus == 1){
-                    FirebaseMessaging.instance.unsubscribeFromTopic('LoveProblems');
+                  if (subStatus == 1) {
+                    FirebaseMessaging.instance
+                        .unsubscribeFromTopic('LoveProblems');
                   }
                 },
               ),
